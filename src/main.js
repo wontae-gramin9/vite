ae67f58e3fa0e3195bd29674daa8aa0b83c9a645
WONTAE\spald <wontae.gramin9@gmail.com>
2024-12-22 20:13:09 +0000

    [CSS] Vite가 ECMA script module로 바로 치환(문자열로 만듦)해서 브라우저에서 바로 사용가능할 수 있게 함

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "./a.js";
-// <script type='module'/>이라서 import로 가능
 import collect from "collect.js";
-// Vite가 node_modules에 설치된 library폴더의 path를 제공해서
-// browser가 request할 수 있게 해주고, 최적화도 해준다
+import "./assets/style.css";
+
 console.log(collect([1, 3, 3, 7]).avg());
