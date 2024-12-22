6352289bb1b3f65322770462572696a62525ad5b
WONTAE\spald <wontae.gramin9@gmail.com>
2024-12-22 11:28:27 +0000

    npm create vite@latest + vite-plugin-inspect ( /__inspect/#/ )

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+// public폴더의 static asset들은 vite에 의해 process되지 않고
+// original form으로 브라우저에 전달된다.
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
