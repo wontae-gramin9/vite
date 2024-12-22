d2cf275df3c3fb312830f6b42510d69d1dd921b6
WONTAE\spald <wontae.gramin9@gmail.com>
2024-12-22 20:21:35 +0000

    [CSS] Vite가 ?inline css파일에 request를 보내고, 그 css파일을 string(ECMAScript Module)으로 parsing해서 default로 export해서 styles로 사용할 수 있다

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./a.js";
 import collect from "collect.js";
-import "./assets/style.css";
+import styles from "./assets/style.css?inline";
 
+console.log("styles:", styles);
 console.log(collect([1, 3, 3, 7]).avg());
