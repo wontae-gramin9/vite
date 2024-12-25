97587a6662e5015b2cc02c427e346a6a14759215
WONTAE\spald <wontae.gramin9@gmail.com>
2024-12-25 16:06:36 +0000

    [Css] 모듈링을 적용하면, 같은 클래스네임이라도 파일 모듈에 따라서 random seed를 줘서 구분한다

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-import "./assets/01.css";
-import "./assets/02.css";
+import stylesA from "./assets/02a.module.css";
+import stylesB from "./assets/02b.module.css";
+
+console.log(stylesA);
+console.log(stylesB);
+
+document.querySelector(
+  ".heading"
+).className = `${stylesA.heading} ${stylesB.heading}`;
